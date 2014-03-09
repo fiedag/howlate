dbed1446b020f8043de0048a046aecc0d34631fe
fiedag <alex.fiedler@internode.on.net>
2014-03-09 16:44:57 +1030

    working authorization details

--- public_html/lib/clickatell.php
+++ public_html/lib/clickatell.php
@@ -3,11 +3,12 @@
 class clickatell {
 
 	private $user = "fiedag";
-	private $password = "94Erx86f";
-	private $api_id = "655502";
+	private $password = "BJ1xmgNO";
+	private $api_id = "195729";
 	private $baseurl ="http://api.clickatell.com";
 
-	public function httpSend($to, $message) {
+	
+	public function httpSend($from, $to, $message) {
 		$text = urlencode($message);
 		// do authorisation call
 		$authurl = "$this->baseurl/http/auth?user=$this->user&password=$this->password&api_id=$this->api_id";
@@ -16,7 +17,7 @@ class clickatell {
 		$sess = explode(":",$ret[0]);
 		if ($sess[0] == "OK") {
 			$sess_id = trim($sess[1]); // remove any whitespace
-			$sendurl = "$this->baseurl/http/sendmsg?session_id=$sess_id&to=$to&text=$text";
+			$sendurl = "$this->baseurl/http/sendmsg?session_id=$sess_id&from=$from&to=$to&text=$text";
 			echo $sendurl . "<br>";
 			$ret = file($sendurl);
 			$send = explode(":",$ret[0]);
