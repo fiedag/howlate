015561379e5d4750bfca8f4b23b00dd5d5c30304
fiedag <alex.fiedler@internode.on.net>
2014-03-19 14:45:15 +1030

    removed comment

--- master/lib/error_handler.php
+++ master/lib/error_handler.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-  abstract class MyErrorTypes
-  {
-  const E_APP_ERROR = 1;
-  const E_API_ERROR = 2;
-  const E_DATA_ERROR = 4;
-  const E_OTHER_ERROR = 0;
-  }
- */
-
 function customErrorHandler($errno, $errstr, $errfile, $errline) {
 
     if (!(error_reporting() & $errno)) {
