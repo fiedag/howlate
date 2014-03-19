226c21e075549c68e3a99424329808aee7d7046d
fiedag <alex.fiedler@internode.on.net>
2014-03-19 15:11:10 +1030

    most recent howlate_user class

--- public_html/master/lib/howlate_user.php
+++ public_html/master/lib/howlate_user.php
@@ -1,13 +1,11 @@
 <?php 
 
-
-
-class howlate_user {
-	public string UserID;  /* unique ID of the table row - internal! */
-	public string DateCreated;
-	public string EmailAddress;
-	public string FullName;
-	public string OrgID;
+class howlate_user extends howlate_basetable {
+	public $UserID;  /* unique ID of the table row - internal! */
+	public $DateCreated;
+	public $EmailAddress;
+	public $FullName;
+	public $OrgID;
 }
 
 
